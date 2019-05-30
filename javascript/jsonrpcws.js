3d0b763615c778826b5f7ab489be91d16140d397
Evgeny Prokopyev <evgeny.prokopyev@megafon.ru>
2019-05-30 10:52:27 +0300

    simple callbacks instead of promises/async/await in javascript example

--- javascript/jsonrpcws.js
+++ javascript/jsonrpcws.js
@@ -3,59 +3,51 @@ class JsonRpcWs {
 	constructor(url) {
 		this.url = url;
 		this.counter = 0;
-		this.requestHandlers = {};
-		this.responseHandlers = {};
+		this.eventHandlers = {};
 	}
 	
 	handle(method, handler) {
-		this.requestHandlers[method] = handler;
+		this.eventHandlers[method] = handler;
 	}
 
-	async open() {
-		return new Promise(resolve => {
-			this.socket = new WebSocket(this.url);
-			this.socket.onopen = () => {
-				console.log('websocket ' + this.socket.url + ' opened');
-				if (this.requestHandlers['OnOpen'])
-					this.requestHandlers['OnOpen']();
-				resolve();
+	open() {
+		this.socket = new WebSocket(this.url);
+		this.socket.onopen = () => {
+			console.log('websocket ' + this.socket.url + ' opened');
+			if (this.eventHandlers['OnOpen'])
+				this.eventHandlers['OnOpen']();
+		}
+		this.socket.onmessage = message => {
+			const data = JSON.parse(message.data);
+			console.log({ response : data });
+			if (data.result) {
+				console.log({ response : data });
+				if (this.eventHandlers['OnSuccess'])
+					this.eventHandlers['OnSuccess'](data.result);
+			} else if (data.error) {
+				console.log({ error : data });
+				if (this.eventHandlers['OnError'])
+					this.eventHandlers['OnError'](data.error);
+			} else if (data.method) {
+				console.log({ event : data });
+				if (this.eventHandlers[data.method])
+					this.eventHandlers[data.method](data.params);
 			}
-			this.socket.onmessage = message => {
-				const data = JSON.parse(message.data);
-				if (data.result) {
-					console.log({ response : data });
-					if (data.id && this.responseHandlers[data.id])
-						this.responseHandlers[data.id](data.result);
-				} else if (data.error) {
-					console.log({ error : data });
-					if (data.id && this.responseHandlers[data.id])
-						this.responseHandlers[data.id](data.error);
-				} else if (data.method) {
-					console.log({ event : data });
-					if (this.requestHandlers[data.method])
-						this.requestHandlers[data.method](data.params);
-				}
-			}
-		})
+		}
 	}
 
-	async request(method, params) {
-		const id = ++this.counter;
-		const request = { id: id, jsonrpc: '2.0', method: method, params: params };
+	request(method, params) {
+		const request = { id: ++this.counter, jsonrpc: '2.0', method: method, params: params };
 		console.log({ request : request });
 		this.socket.send(JSON.stringify(request));
-		return new Promise(resolve => this.responseHandlers[id] = resolve);
 	}
 
-	async close() {
-		return new Promise(resolve => {
-			this.socket.onclose = () => {
-				console.log('websocket ' + this.socket.url + ' closed');
-				if (this.requestHandlers['OnClose'])
-					this.requestHandlers['OnClose']();
-				resolve();
-			}
-			this.socket.close();
-		})
+	close() {
+		this.socket.onclose = () => {
+			console.log('websocket ' + this.socket.url + ' closed');
+			if (this.eventHandlers['OnClose'])
+				this.eventHandlers['OnClose']();
+		}
+		this.socket.close();
 	}
 }
